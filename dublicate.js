8e92cddd7dcbaa00a611b96c637e92a9a2ab2776
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2023-10-27 21:23:12 +0600

    create dublicate.js file for learning the process of removing dublicate file and divisible.js file

---
+++ dublicate.js
@@ -0,0 +1,14 @@
+const friendsName = ["shajib","tipu", "sayem", "shajib", "sharif", "tipu", "srabon", "sharif", "shafikul", "monir", " tipu", "sharif"];
+console.log(friendsName);
+function isDublicate(names){
+    const array = [];
+    for(let i = 0; i <= names.length - 1; i++){
+        const element = names[i];
+        if(array.includes(element) === false){
+            array.push(element);
+        }
+    }
+    return array;
+}
+const result = isDublicate(friendsName);
+console.log(result);
